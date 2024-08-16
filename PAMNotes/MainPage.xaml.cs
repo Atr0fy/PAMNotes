0555cc5f6fe2b0654c2f0abe83914329f2a81ecc
lucasns06 <2006lucasns@gmail.com>
2024-08-16 19:38:55 -0300

    Repositorio enviado

---
+++ PAMNotes/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace PAMNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        string filePath = Path.Combine(FileSystem.AppDataDirectory,"Notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(filePath))
+            {
+                NoteEditor.Text = File.ReadAllText(filePath);
+            }
+        }
+        
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string texto = NoteEditor.Text;
+            TextLabel.Text = "Arquivo foi salvo em: " + filePath;
+            File.WriteAllText(filePath, texto);
+            DisplayAlert("Alerta", "Arquivo salvo", "ok");
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            NoteEditor.Text = "";
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                DisplayAlert("Alerta", "Arquivo deletado", "OK");
+                NoteEditor.Text = string.Empty;
+                TextLabel.Text = "";
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Arquivo não encontrado", "OK");
+            }
+            TextLabel.Text = "";
+        }
+    }
+
+}
