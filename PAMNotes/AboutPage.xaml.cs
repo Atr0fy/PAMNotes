706d84f8b419edfab990b1c04a6aab80198aae00
lucasns06 <2006lucasns@gmail.com>
2024-08-16 22:18:41 -0300

    Pagina sobre

---
+++ PAMNotes/AboutPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace PAMNotes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void AboutButton_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://github.com/lucasns06");
+    }
+
+    private async void SteamButton_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://steamcommunity.com/id/Atrofy/");
+    }
+}
